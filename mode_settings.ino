b7662a4172ed184ddc7f9d410fb6f4dfcf4e927e
Noctaro <nocturn@quadrocopter.at>
2016-11-29 15:50:38 +0100

    Add files via upload

--- mode_settings.ino
+++ mode_settings.ino
@@ -50,18 +50,18 @@ void Mode1_active()
   optimaleLuftfeuchte = 68;
  
  //*********************************************************************************************************
- //UHRZEITEN für Bewässerung für Mode 1 hier eintragen! (Zum deaktivieren einer Zeit einfach 99 eintragen)
+ //UHRZEITEN für Bewässerung für Mode 1 hier eintragen! (10 verschiedeme Stunden stehen zur Verfügung (Zum deaktivieren einer Zeit einfach 99 eintragen)
  //*********************************************************************************************************
-  int water_hour_01 = 99;
-  int water_hour_02 = 99;
-  int water_hour_03 = 99;
-  int water_hour_04 = 99;
-  int water_hour_05 = 99;
-  int water_hour_06 = 99;
-  int water_hour_07 = 99;
-  int water_hour_08 = 99;
-  int water_hour_09 = 99;
-  int water_hour_10 = 99;
+  water_hour_01 = 99;
+  water_hour_02 = 99;
+  water_hour_03 = 99;
+  water_hour_04 = 99;
+  water_hour_05 = 99;
+  water_hour_06 = 99;
+  water_hour_07 = 99;
+  water_hour_08 = 99;
+  water_hour_09 = 99;
+  water_hour_10 = 99;
   //*******************************************************************************************
   long flush_time_secounds = 90;  //Dauer der Wasserzufuhr bei dem Bewässern
   //*********************************************************************************************************
