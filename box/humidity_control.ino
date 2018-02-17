c34b261789f726b282dd3d00ebf71dfdb5728c31
Noctaro <nocturn@quadrocopter.at>
2018-02-17 16:49:01 +0100

    Update humidity_control.ino

--- box/humidity_control.ino
+++ box/humidity_control.ino
@@ -19,24 +19,25 @@ if (h < optimaleLuftfeuchte)
 feuchtewert--;
 }
 
-if (zaehler == 15)                                  //Wenn der Messzähler 15 Messdurchgänge erreicht hat                                  
+if (zaehler == Messdurchgaenge)                                  //Wenn der MesszÃ¤hler Messdurchgaenge MessdurchgÃ¤nge erreicht hat                                  
  {
   
-    //Der Schaltvorgang für den Befeuchter
-    if (h <= minLuftfeuchte && feuchtewert < -14)    //Wenn die aktuelle Feuchte niedriger als die Optimale Luftfeuchte ist und feuchtewert dies 15 mal bestätigt hat.
+    //Der Schaltvorgang fÃ¼r den Befeuchter
+    if (h <= minLuftfeuchte && feuchtewert <= Messdurchgaenge)    //Wenn die aktuelle Feuchte niedriger als die Optimale Luftfeuchte ist und feuchtewert dies Messdurchgaenge_negativ mal bestÃ¤tigt hat.
     {
     digitalWrite(relaitPin1, HIGH);          //Schalte Relait Pin 1 ein
     relait1check = 1;
     humidity_balancer_ausgabe();
     }
   
-    if (h >= optimaleLuftfeuchte && feuchtewert > 14)    //Wenn die aktuelle Feuchte höher als die Optimale Luftfeuchte ist und feuchtewert dies 15 mal bestätigt hat.
+    if (h >= optimaleLuftfeuchte && feuchtewert >= Messdurchgaenge && Absaugung_aktiv == 0)    //Wenn die aktuelle Feuchte hÃ¶her als die Optimale Luftfeuchte ist und feuchtewert dies Messdurchgaenge mal bestÃ¤tigt hat. Und die Absaugung nicht aktiv ist.
     {
     digitalWrite(relaitPin1, LOW);           //Schalte Relait Pin 1 aus
     relait1check = 0;
     humidity_balancer_ausgabe2();
     }
-
+  
+ 
   feuchtewert = 0;
   }
 }
