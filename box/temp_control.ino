f33acd06eff138f430700eac5f2458630feac74c
Noctaro <nocturn@quadrocopter.at>
2018-02-23 23:01:58 +0100

    change relait switch type

--- box/temp_control.ino
+++ box/temp_control.ino
@@ -17,8 +17,8 @@ if (zaehler == Messdurchgaenge)
   if (t <= minTemperatur && t < optimaleTemperatur && temperaturwert == Messdurchgaenge_negativ)
   {
   Heizung_ausgabe();
-  //digitalWrite(relaitPin3, HIGH);         //Schalte relaitPin3 ein      
-  pinMode(relaitPin3, INPUT);
+  digitalWrite(relaitPin3, HIGH);         //Schalte relaitPin3 ein      
+  //pinMode(relaitPin3, INPUT);
   relait3check = 1;
   delay(100);
   }
@@ -26,8 +26,8 @@ if (zaehler == Messdurchgaenge)
   if (t >= optimaleTemperatur && temperaturwert == Messdurchgaenge)
   {
   Heizung_ausgabe2();
-  //digitalWrite(relaitPin3, LOW);         //Schalte relaitPin3 aus 
-  pinMode(relaitPin3, OUTPUT);
+  digitalWrite(relaitPin3, LOW);         //Schalte relaitPin3 aus 
+  //pinMode(relaitPin3, OUTPUT);
   relait3check = 0;    
   delay(100);
   }
