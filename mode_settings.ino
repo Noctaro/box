1d8d48963447c1f96a16e175449f4a5fe684e271
Noctaro <nocturn@quadrocopter.at>
2018-02-26 00:21:52 +0100

    enable watchdog

--- mode_settings.ino
+++ mode_settings.ino
@@ -213,7 +213,7 @@ void Mode1_settings_active()
  
   //Zeitgesteuert -Bei Tag
   //*******************************************//
-  if (hour_global < nachtbeginn_stunde || hour_global > tagbeginn_stunde) //Bei Tag
+  if (hour_global < nachtbeginn_stunde || hour_global >= tagbeginn_stunde) //Bei Tag
   {
   maxTemperatur = 27;
   optimaleTemperatur = 26;
@@ -233,7 +233,7 @@ void Mode1_settings_active()
   
   //Zeitgesteuert - Bei Nacht
   //*******************************************//
-  if (hour_global >= nachtbeginn_stunde || hour_global <= tagbeginn_stunde) //Bei Nacht
+  if (hour_global >= nachtbeginn_stunde || hour_global < tagbeginn_stunde) //Bei Nacht
   {
   maxTemperatur = 27;
   optimaleTemperatur = 26;
