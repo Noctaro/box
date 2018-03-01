982973d2ca46d492220ff26d792a5a083c9650f6
Noctaro <nocturn@quadrocopter.at>
2018-03-01 12:16:57 +0100

    relais fix

--- box/humidity_control.ino
+++ box/humidity_control.ino
@@ -35,7 +35,6 @@ if (zaehler == Messdurchgaenge)                                  //Wenn der Mess
     if (h >= optimaleLuftfeuchte && feuchtewert >= Messdurchgaenge && Absaugung_aktiv == 0)    //Wenn die aktuelle Feuchte höher als die Optimale Luftfeuchte ist und feuchtewert dies Messdurchgaenge mal bestätigt hat. Und die Absaugung nicht aktiv ist.
     {
     relais_1_off();           //Schalte Relait Pin 1 aus
-    relais1check = 0;
     humidity_balancer_ausgabe2();
     int relais_1_guard = unix_secounds;
     }
@@ -45,15 +44,19 @@ if (zaehler == Messdurchgaenge)                                  //Wenn der Mess
   }
 
   //TESTING BEFEUCHTUNG BEIM LÜFTEN
-    if(water_with_air == 1 && t >= minTemperatur && h < optimaleLuftfeuchte && relais_1_guard <= unix_secounds+relais_1_guard_time)
+  
+    if(water_with_air == 1 && t >= minTemperatur && h < optimaleLuftfeuchte && relais_1_guard <= unix_secounds+relais_1_guard_time) // Wenn die temperatur größer oder gleich der minimalen Temperatur ist und die Luftfeuchte kleiner als die optimale Feuchte ist 
       {
-      relais_1_on();
+      relais_1_on();              //Schalte befeuchtung ein
+      int relais_1_guard = unix_secounds;
       Absaugung_aktiv = 1;
       }
     
-    if(water_with_air == 1 && h >= maxLuftfeuchte && relais_1_guard <= unix_secounds+relais_1_guard_time)
+    if(water_with_air == 1 && h >= maxLuftfeuchte && relais_1_guard <= unix_secounds+relais_1_guard_time) //Wenn die Luftfeuchte größer oder gleich der maximalen Feuchte ist
       {
-      relais_1_off();
+      relais_1_off();            //Schalte befeuchtung aus
+      int relais_1_guard = unix_secounds;
+      Absaugung_aktiv = 0;
       }  
 
 }
