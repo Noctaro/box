5ad94efdd8c753848b5d8e24b98cb4aa5ca5a567
Noctaro <nocturn@quadrocopter.at>
2018-10-07 16:19:27 +0200

    Update humidity_control.ino

--- box/humidity_control.ino
+++ box/humidity_control.ino
@@ -15,19 +15,19 @@ if (h < optimaleLuftfeuchte)
 feuchtewert--;
 }
 
-if (zaehler == Messdurchgaenge)                                  //Wenn der MesszÃ¤hler Messdurchgaenge MessdurchgÃ¤nge erreicht hat                                  
+if (zaehler == Messdurchgaenge)                                  //Wenn der MesszÃƒÂ¤hler Messdurchgaenge MessdurchgÃƒÂ¤nge erreicht hat                                  
  {
 
   if (h_w_compenstation_active == 0)
     {
-      //Der Schaltvorgang fÃ¼r den Befeuchter
-      if (h <= minLuftfeuchte && feuchtewert <= Messdurchgaenge)    //Wenn die aktuelle Feuchte niedriger als die Optimale Luftfeuchte ist und feuchtewert dies Messdurchgaenge_negativ mal bestÃ¤tigt hat.
+      //Der Schaltvorgang fÃƒÂ¼r den Befeuchter
+      if (h <= minLuftfeuchte && feuchtewert <= Messdurchgaenge)    //Wenn die aktuelle Feuchte niedriger als die Optimale Luftfeuchte ist und feuchtewert dies Messdurchgaenge_negativ mal bestÃƒÂ¤tigt hat.
       {
       relais_1_on();          //Schalte Relait Pin 1 ein
       humidity_balancer_ausgabe();
       }
   
-      if (h >= optimaleLuftfeuchte && feuchtewert >= Messdurchgaenge && Absaugung_aktiv == 0)    //Wenn die aktuelle Feuchte hÃ¶her als die Optimale Luftfeuchte ist und feuchtewert dies Messdurchgaenge mal bestÃ¤tigt hat. Und die Absaugung nicht aktiv ist.
+      if (h >= optimaleLuftfeuchte && feuchtewert >= Messdurchgaenge && Absaugung_aktiv == 0)    //Wenn die aktuelle Feuchte hÃƒÂ¶her als die Optimale Luftfeuchte ist und feuchtewert dies Messdurchgaenge mal bestÃƒÂ¤tigt hat. Und die Absaugung nicht aktiv ist.
       {
       relais_1_off();           //Schalte Relait Pin 1 aus
       humidity_balancer_ausgabe2();
@@ -62,7 +62,7 @@ void water_heat()
 if (heat_water_compensation == 1)
   {
   
-  if (maxTemperatur < t && h < maxLuftfeuchte)  //Nur aktiv wenn die Temperatur überschritten wird, aber nicht wenn die maximale Luftfeuchte überschritten wird.
+  if (maxTemperatur < t && h < maxLuftfeuchte)  //Nur aktiv wenn die Temperatur Ã¼berschritten wird, aber nicht wenn die maximale Luftfeuchte Ã¼berschritten wird.
     {
       h_w_compenstation_active = 1;
       relais_1_on();
@@ -70,7 +70,7 @@ if (heat_water_compensation == 1)
       delay(1000);
     }
     
-    if(h_w_compenstation_active == 1 && maxTemperatur > t)
+    if(h_w_compenstation_active == 1 && maxTemperatur > t || h_w_compenstation_active == 1 && maxLuftfeuchte <= h)
     {
       h_w_compenstation_active = 0;
       relais_1_off();
@@ -82,17 +82,17 @@ if (heat_water_compensation == 1)
 //************************
 
 //************************
-//TESTING BEFEUCHTUNG BEIM LÃœFTEN
+//TESTING BEFEUCHTUNG BEIM LÃƒÅ“FTEN
 //************************  
   void air_water()
   {
-    if(water_with_air == 1 && t >= minTemperatur && h <= optimaleLuftfeuchte) // Wenn die temperatur grÃ¶ÃŸer oder gleich der minimalen Temperatur ist und die Luftfeuchte kleiner als die optimale Feuchte ist 
+    if(water_with_air == 1 && t >= minTemperatur && h <= optimaleLuftfeuchte) // Wenn die temperatur grÃƒÂ¶ÃƒÅ¸er oder gleich der minimalen Temperatur ist und die Luftfeuchte kleiner als die optimale Feuchte ist 
       {
       relais_1_on();              //Schalte befeuchtung ein
       Absaugung_aktiv = 1;
       }
     
-    if(water_with_air == 1 && h > optimaleLuftfeuchte) //Wenn die Luftfeuchte grÃ¶ÃŸer oder gleich der maximalen Feuchte ist
+    if(water_with_air == 1 && h > optimaleLuftfeuchte) //Wenn die Luftfeuchte grÃƒÂ¶ÃƒÅ¸er oder gleich der maximalen Feuchte ist
       {
       relais_1_off();            //Schalte befeuchtung aus
       Absaugung_aktiv = 0;
