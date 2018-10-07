744f081e91df19541d6cd00a3d108221ad60c10e
Noctaro <nocturn@quadrocopter.at>
2018-10-07 16:22:08 +0200

    Update relais.h

--- box/relais.h
+++ box/relais.h
@@ -4,10 +4,10 @@
 //*********************************************************************************************************
 //RELAIS PINS
 //*********************************************************************************************************
-#define relaisPin1 5 //Definiere den Namen und Pin fÃ¼r das 1. relais - Feuchte
-#define relaisPin2 6 //Definiere den Namen und Pin fÃ¼r das 2. relais - Wasser  
-#define relaisPin3 7 //Definiere den Namen und Pin fÃ¼r das 3. relais - Heizung 
-#define relaisPin4 4 //Definiere den Namen und Pin fÃ¼r das 4. relais - Abluft
+#define relaisPin1 4 //Definiere den Namen und Pin fÃƒÂ¼r das 1. relais - Feuchte
+#define relaisPin2 5 //Definiere den Namen und Pin fÃƒÂ¼r das 2. relais - Wasser  
+#define relaisPin3 6 //Definiere den Namen und Pin fÃƒÂ¼r das 3. relais - Heizung 
+#define relaisPin4 7 //Definiere den Namen und Pin fÃƒÂ¼r das 4. relais - Abluft
 
 //*********************************************************************************************************
 //Relais Globale Variablen
@@ -30,16 +30,16 @@ int relais_4_guard = 0;
 
 void relaisinit()
 {
-  digitalWrite(relaisPin1, LOW);         //Schalte relaisPin1 aus 
-  digitalWrite(relaisPin2, LOW);         //Schalte relaisPin2 aus
-  digitalWrite(relaisPin3, LOW);         //Schalte relaisPin3 aus - umgekehrte schaltlogik
-  digitalWrite(relaisPin4, LOW); 
+  digitalWrite(relaisPin1, HIGH);         //Schalte relaisPin1 aus 
+  digitalWrite(relaisPin2, HIGH);         //Schalte relaisPin2 aus
+  digitalWrite(relaisPin3, HIGH);         //Schalte relaisPin3 aus 
+  digitalWrite(relaisPin4, HIGH); 
   
   //PIN Modus festlegen  
-  pinMode(relaisPin1, OUTPUT); //Setze den Steuerpin fÃ¼r relais 1 als Ausgang
-  pinMode(relaisPin2, OUTPUT); //Setze den Steuerpin fÃ¼r relais 2 als Ausgang
-  pinMode(relaisPin3, OUTPUT); //Setze den Steuerpin fÃ¼r relais 3 als Ausgang
-  pinMode(relaisPin4, OUTPUT); //Setze den Steuerpin fÃ¼r relais 4 als Ausgang
+  pinMode(relaisPin1, OUTPUT); //Setze den Steuerpin fÃƒÂ¼r relais 1 als Ausgang
+  pinMode(relaisPin2, OUTPUT); //Setze den Steuerpin fÃƒÂ¼r relais 2 als Ausgang
+  pinMode(relaisPin3, OUTPUT); //Setze den Steuerpin fÃƒÂ¼r relais 3 als Ausgang
+  pinMode(relaisPin4, OUTPUT); //Setze den Steuerpin fÃƒÂ¼r relais 4 als Ausgang
 }
 
 
@@ -47,12 +47,15 @@ void relaisinit()
 //*********************************************************************************************************
 //RELAIS 1
 //*********************************************************************************************************
-void relais_1_on()
+//Umgekehrte Logik fÃ¼r SSR Relais aktiv
+//ACHTUNG RELAIS CHECK WURDE AUCH ANGEPASST
+
+void relais_1_off()
 {
-  if(relais1check==0 && relais_1_guard <= unix_secounds+relais_1_guard_time)
+  if(relais1check==1 && relais_1_guard <= unix_secounds+relais_1_guard_time)
   {     
   digitalWrite(relaisPin1, HIGH);
-  relais1check = 1;
+  relais1check = 0;
   relais_1_guard = unix_secounds;
   delay(1000);
   }
@@ -60,12 +63,12 @@ void relais_1_on()
 }
 
 
-void relais_1_off()
+void relais_1_on()
 {
-  if(relais1check==1 && relais_1_guard <= unix_secounds+relais_1_guard_time)
+  if(relais1check==0 && relais_1_guard <= unix_secounds+relais_1_guard_time)
   {     
   digitalWrite(relaisPin1, LOW);
-  relais1check = 0;
+  relais1check = 1;
   relais_1_guard = unix_secounds;
   delay(1000);
   }
@@ -77,12 +80,12 @@ void relais_1_off()
 //RELAIS 2
 //*********************************************************************************************************
 
-void relais_2_on()
+void relais_2_off()
 {
-  if(relais2check==0 && relais_2_guard <= unix_secounds+relais_2_guard_time)
+  if(relais2check==1 && relais_2_guard <= unix_secounds+relais_2_guard_time)
   {     
   digitalWrite(relaisPin2, HIGH);
-  relais2check = 1;
+  relais2check = 0;
   relais_2_guard = unix_secounds;
   delay(1000);
   }
@@ -90,12 +93,12 @@ void relais_2_on()
 }
 
 
-void relais_2_off()
+void relais_2_on()
 {
-  if(relais2check==1 && relais_2_guard <= unix_secounds+relais_2_guard_time)
+  if(relais2check==0 && relais_2_guard <= unix_secounds+relais_2_guard_time)
   {     
   digitalWrite(relaisPin2, LOW);
-  relais2check = 0;
+  relais2check = 1;
   relais_2_guard = unix_secounds;
   delay(1000);
   }
@@ -107,12 +110,12 @@ void relais_2_off()
 //RELAIS 3
 //*********************************************************************************************************
 
-void relais_3_on()
+void relais_3_off()
 {
-  if(relais3check==0 && relais_3_guard <= unix_secounds+relais_3_guard_time)
+  if(relais3check==1 && relais_3_guard <= unix_secounds+relais_3_guard_time)
   {     
   digitalWrite(relaisPin3, HIGH);
-  relais3check = 1;
+  relais3check = 0;
   relais_3_guard = unix_secounds;
   delay(1000);
   }
@@ -120,12 +123,12 @@ void relais_3_on()
 }
 
 
-void relais_3_off()
+void relais_3_on()
 {
-  if(relais3check==1 && relais_3_guard <= unix_secounds+relais_3_guard_time)
+  if(relais3check==0 && relais_3_guard <= unix_secounds+relais_3_guard_time)
   {     
   digitalWrite(relaisPin3, LOW);
-  relais3check = 0;
+  relais3check = 1;
   relais_3_guard = unix_secounds;
   delay(1000);
   }
@@ -137,12 +140,12 @@ void relais_3_off()
 //RELAIS 4
 //*********************************************************************************************************
 
-void relais_4_on()
+void relais_4_off()
 {
-  if(relais4check==0 && relais_4_guard <= unix_secounds+relais_4_guard_time)
+  if(relais4check==1 && relais_4_guard <= unix_secounds+relais_4_guard_time)
   {     
   digitalWrite(relaisPin4, HIGH);
-  relais4check = 1;
+  relais4check = 0;
   relais_4_guard = unix_secounds;
   delay(1000);
   }
@@ -150,12 +153,12 @@ void relais_4_on()
 }
 
 
-void relais_4_off()
+void relais_4_on()
 {
-  if(relais4check==1 && relais_4_guard <= unix_secounds+relais_4_guard_time)
+  if(relais4check==0 && relais_4_guard <= unix_secounds+relais_4_guard_time)
   {     
   digitalWrite(relaisPin4, LOW);
-  relais4check = 0;
+  relais4check = 1;
   relais_4_guard = unix_secounds;
   delay(1000);
   }
